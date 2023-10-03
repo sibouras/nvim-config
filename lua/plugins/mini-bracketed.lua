b7f782cdede232cdea4b8f5ff9f97635b54e76be
sibouras <sibouras@users.noreply.github.com>
2023-10-03 20:06:33 +0100

    new mini.bracketed pugin and make some movements repeatable with `;` and ',`

---
+++ lua/plugins/mini-bracketed.lua
@@ -0,0 +1,22 @@
+return {
+  'echasnovski/mini.bracketed',
+  event = 'VeryLazy',
+  opts = {
+    file = { suffix = 'r', options = { wrap = false } },
+    buffer = { suffix = 'z', options = { wrap = false } },
+    oldfile = { suffix = 'o', options = { wrap = false } },
+    conflict = { suffix = 'x', options = {} },
+    location = { suffix = 'l', options = {} },
+    yank = { suffix = 'y', options = {} },
+    window = { suffix = 'w', options = {} },
+    -- using these with Repeat movement with ; and ,
+    comment = { suffix = 'c', options = { add_to_jumplist = true } },
+    indent = { suffix = 'i', options = { add_to_jumplist = true } },
+    treesitter = { suffix = 't', options = { add_to_jumplist = true } },
+    -- disable these
+    diagnostic = { suffix = '', options = {} },
+    jump = { suffix = '', options = {} },
+    quickfix = { suffix = '', options = {} },
+    undo = { suffix = '', options = {} },
+  },
+}
