0c4b1504029595cb69f47c2ff6dd9d54a11ef1e2
ala <marzoukala2018@gmail.com>
2023-10-05 11:53:47 +0100

    add group to autocmd, new autocmds to change tab title and delete entry from quickfix list

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,26 +1,19 @@
--- nvim builtin highlight
--- vim.cmd([[
---   augroup highlight_yank
---     autocmd!
---     autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=200}
---   augroup END
--- ]])
--- highlight on yank with lua
+local function augroup(name)
+  return vim.api.nvim_create_augroup('MyGroup_' .. name, { clear = true })
+end
+
+-- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  group = augroup('highlight_yank'),
   callback = function()
     vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })
   end,
 })
 
--- disable auto-comment on o and O and enter
--- vim.cmd([[
---   augroup formatoptions
---     autocmd!
---     autocmd BufEnter * setlocal formatoptions -=o formatoptions -=r
---   augroup END
--- ]])
--- with lua
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  desc = 'disable auto-comment on o and O and enter',
+  group = augroup('formatoptions'),
   callback = function()
     vim.cmd('set formatoptions-=cro')
     vim.cmd([[set formatexpr=\"]]) -- empty formatexpr so gq uses formatoptions
@@ -48,9 +41,7 @@ vim.cmd([[
 ]])
 
 -- Automatically equalize splits when Vim is resized
-vim.cmd([[
-  autocmd VimResized * wincmd =
-]])
+vim.cmd([[autocmd VimResized * wincmd =]])
 
 -- automatically open quickfix window and don't jump to first match
 vim.cmd([[command! -nargs=+ Grep execute 'silent grep! <args>' | copen]])
@@ -98,28 +89,30 @@ endif
 
 -- source: https://github.com/ecosse3/nvim/blob/master/lua/config/autocmds.lua
 -- Disable diagnostics in node_modules (0 is current buffer only)
-vim.api.nvim_create_autocmd(
-  { 'BufRead', 'BufNewFile' },
-  { pattern = '*/node_modules/*', command = 'lua vim.diagnostic.disable(0)' }
-)
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  desc = 'Disable diagnostics in node_modules',
+  group = augroup('disable_diagnostics'),
+  pattern = '*/node_modules/*',
+  command = 'lua vim.diagnostic.disable(0)',
+})
 
 -- Enable spell checking for certain file types
 -- vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" })
 
--- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = { 'qf', 'help', 'man', 'lspinfo', 'spectre_panel', 'startuptime', 'Redir' },
-  callback = function()
-    vim.cmd([[
-      nnoremap <silent> <buffer> q :close<CR>
-      set nobuflisted
-    ]])
+  desc = 'close some filetypes with <q>',
+  pattern = { 'qf', 'help', 'man', 'spectre_panel', 'startuptime', 'Redir' },
+  group = augroup('close_with_q'),
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<Cmd>close<CR>', { buffer = event.buf, silent = true })
   end,
 })
 
--- Remove statusline and tabline when in Alpha
 vim.api.nvim_create_autocmd({ 'User' }, {
+  desc = 'Remove statusline and tabline when in Alpha',
   pattern = { 'AlphaReady' },
+  group = augroup('alpha'),
   callback = function()
     vim.cmd([[
       " set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
@@ -128,23 +121,57 @@ vim.api.nvim_create_autocmd({ 'User' }, {
   end,
 })
 
--- Set wrap and spell in markdown and gitcommit
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = { "gitcommit", "markdown" },
---   callback = function()
---     vim.opt_local.wrap = true
---     vim.opt_local.spell = true
---   end,
--- })
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('wrap_spell'),
+  pattern = { 'gitcommit' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
 
--- insert mode when switching to terminal
 vim.api.nvim_create_autocmd('WinEnter', {
+  desc = 'go to insert mode when switching to terminal',
+  group = augroup('terminal'),
   pattern = 'term://*',
   command = 'startinsert',
 })
 
--- cursorline in harpoon
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'cursorline in grapple',
+  group = augroup('grapple'),
+  pattern = 'grapple',
+  callback = function()
+    vim.opt_local.cursorline = true
+    vim.keymap.set('n', '<M-e>', '<Cmd>close<CR>', { buffer = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd('SessionLoadPost', {
+  desc = 'change tab title to directory name when loading session',
+  group = augroup('change_title'),
+  callback = function()
+    -- return the tail path of the current working directory
+    local dirname = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+    vim.uv.set_process_title(dirname .. ' - nvim')
+  end,
+})
+
+-- from: https://github.com/zdcthomas/yakko_wakko/blob/main/config/nvim/lua/autocmds.lua
+-- delete entries from a quickfix list with `dd`
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = 'harpoon',
-  command = 'setlocal cursorline',
+  desc = 'Delete entry from Quickfix list',
+  group = augroup('quickfix'),
+  pattern = { 'qf' },
+  callback = function()
+    vim.keymap.set('n', 'dd', function()
+      local current_quick_fix_index = vim.fn.line('.')
+      local quickfix_list = vim.fn.getqflist()
+      table.remove(quickfix_list, current_quick_fix_index)
+      vim.fn.setqflist(quickfix_list, 'r')
+      vim.fn.execute(current_quick_fix_index .. 'cfirst')
+      vim.cmd('copen')
+    end, { buffer = true })
+  end,
 })
