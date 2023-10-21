070750b9baeefd797239b3feebfefe6ca0977cd3
ala <marzoukala2018@gmail.com>
2023-10-21 08:28:39 +0100

    feat(mini.files): add keymaps to toggle preview and split windows

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -32,6 +32,16 @@ return {
         return entry.name ~= '.git'
       end,
     },
+    windows = {
+      -- Whether to show preview of file/directory under cursor
+      preview = false,
+      -- Width of focused window
+      width_focus = 40,
+      -- Width of non-focused window
+      width_nofocus = 20,
+      -- Width of preview window
+      width_preview = 25,
+    },
   },
   config = function(_, opts)
     local minifiles = require('mini.files')
@@ -56,7 +66,7 @@ return {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
         -- Tweak left-hand side of mapping to your liking
-        vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = args.data.buf_id })
+        vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = args.data.buf_id, desc = 'show/hide dot-files' })
       end,
     })
 
@@ -72,7 +82,7 @@ return {
     vim.api.nvim_create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
-        vim.keymap.set('n', 'gd', files_set_cwd, { buffer = args.data.buf_id })
+        vim.keymap.set('n', 'gd', files_set_cwd, { buffer = args.data.buf_id, desc = 'set cwd' })
       end,
     })
 
@@ -83,7 +93,54 @@ return {
         vim.keymap.set('n', 'go', function()
           local cur_entry_path = minifiles.get_fs_entry().path
           os.execute('start "" "' .. cur_entry_path .. '"')
-        end, { buffer = args.data.buf_id })
+        end, { buffer = args.data.buf_id, desc = 'open with system application' })
+      end,
+    })
+
+    -- show/hide preview window
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local is_preview = minifiles.config.windows.preview
+        vim.keymap.set('n', 'gp', function()
+          is_preview = not is_preview
+          minifiles.refresh({ windows = { preview = is_preview } })
+        end, { buffer = args.data.buf_id, desc = 'show/hide preview' })
+      end,
+    })
+
+    -- Add minifiles split keymaps
+    local function map_split(buf_id, lhs, direction)
+      local function rhs()
+        local window = minifiles.get_target_window()
+        -- Noop if the explorer isn't open or the cursor is on a directory.
+        if window == nil or minifiles.get_fs_entry().fs_type == 'directory' then
+          return
+        end
+
+        -- Make a new window and set it as target.
+        local new_target_window
+        vim.api.nvim_win_call(window, function()
+          vim.cmd(direction .. ' split')
+          new_target_window = vim.api.nvim_get_current_win()
+        end)
+
+        minifiles.set_target_window(new_target_window)
+        -- Go in and close the explorer.
+        minifiles.go_in()
+        minifiles.close()
+      end
+
+      vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = 'Split ' .. string.sub(direction, 12) })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      desc = 'Add minifiles split keymaps',
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        map_split(buf_id, '<C-h>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
       end,
     })
   end,
