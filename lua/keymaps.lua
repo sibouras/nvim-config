5852a7244003f2bc801f233b3ccf976af90ef95e
sibouras <sibouras@users.noreply.github.com>
2023-11-01 19:55:16 +0100

    feat: terminal keymaps and autocmds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -398,6 +398,11 @@ map({ 'i', 'n' }, '<M-C-S-F7>', function()
   vim.g.cmp_active = not vim.g.cmp_active
 end, { desc = 'toggle cmp' })
 
+-- Terminal Mappings
+map('t', '`', '<c-\\><c-n>', { desc = 'Enter Normal Mode' })
+map('t', '<C-r>', [['<C-\><C-n>"' . nr2char(getchar()) . 'pi']], { expr = true })
+map('t', '<M-p>', [[<C-\><C-n>"+pi]])
+
 ----------------------------------
 --- functions
 ----------------------------------
