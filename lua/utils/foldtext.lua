4c59202ca7b5735b8e3bf5453edefdaacbea5e37
ala <marzoukala2018@gmail.com>
2023-11-02 23:41:11 +0100

    better foldtext from small.nvim

--- lua/utils/foldtext.lua
+++ lua/utils/foldtext.lua
@@ -1,130 +1,59 @@
--- from https://github.com/Wansmer/nvim-config/blob/main/lua/modules/foldtext.lua
----Based on https://www.reddit.com/r/neovim/comments/16sqyjz/finally_we_can_have_highlighted_folds/
----Updated with vim.treesitter._fold.foldtext()
-
-local function parse_line(linenr)
-  local bufnr = vim.api.nvim_get_current_buf()
-
-  local line = vim.api.nvim_buf_get_lines(bufnr, linenr - 1, linenr, false)[1]
-  if not line then
-    return nil
+-- from: https://github.com/altermo/small.nvim/blob/main/lua/small/foldtext.lua
+local M = { conf = { treesitter = true } }
+function M.GetTreesitterFoldText(just, fallback)
+  if not M.conf.treesitter then
+    fallback = fallback:sub(just) .. ' '
+    local len = #vim.str_utf_pos(fallback)
+    return { { fallback } }, len
   end
-
-  local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
-  if not ok then
-    return nil
+  local foldtext = vim.treesitter.foldtext()
+  if type(foldtext) == 'string' then
+    fallback = fallback:sub(just) .. ' '
+    local len = #vim.str_utf_pos(fallback)
+    return { { fallback } }, len
   end
-
-  local query = vim.treesitter.query.get(parser:lang(), 'highlights')
-  if not query then
-    return nil
-  end
-
-  local tree = parser:parse({ linenr - 1, linenr })[1]
-
-  local result = {}
-
-  local line_pos = 0
-
-  for id, node, metadata in query:iter_captures(tree:root(), 0, linenr - 1, linenr) do
-    local name = query.captures[id]
-    local start_row, start_col, end_row, end_col = node:range()
-
-    local priority = tonumber(metadata.priority or vim.highlight.priorities.treesitter)
-
-    if start_row == linenr - 1 and end_row == linenr - 1 then
-      -- check for characters ignored by treesitter
-      if start_col > line_pos then
-        table.insert(result, {
-          line:sub(line_pos + 1, start_col),
-          { { 'Folded', priority } },
-          range = { line_pos, start_col },
-        })
+  table.insert(foldtext, { ' ' })
+  if just ~= 1 then
+    while just > 0 do
+      just = just - 1
+      if foldtext[1][1] == '' then
+        table.remove(foldtext, 1)
+      else
+        foldtext[1][1] = foldtext[1][1]:sub(2)
       end
-      line_pos = end_col
-
-      local text = line:sub(start_col + 1, end_col)
-      table.insert(result, { text, { { '@' .. name, priority } }, range = { start_col, end_col } })
     end
   end
-
-  local i = 1
-  while i <= #result do
-    -- find first capture that is not in current range and apply highlights on the way
-    local j = i + 1
-    while j <= #result and result[j].range[1] >= result[i].range[1] and result[j].range[2] <= result[i].range[2] do
-      for k, v in ipairs(result[i][2]) do
-        if not vim.tbl_contains(result[j][2], v) then
-          table.insert(result[j][2], k, v)
-        end
-      end
-      j = j + 1
-    end
-
-    -- remove the parent capture if it is split into children
-    if j > i + 1 then
-      table.remove(result, i)
-    else
-      -- highlights need to be sorted by priority, on equal prio, the deeper nested capture (earlier
-      -- in list) should be considered higher prio
-      if #result[i][2] > 1 then
-        table.sort(result[i][2], function(a, b)
-          return a[2] < b[2]
-        end)
-      end
-
-      result[i][2] = vim.tbl_map(function(tbl)
-        return tbl[1]
-      end, result[i][2])
-      result[i] = { result[i][1], result[i][2] }
-
-      i = i + 1
-    end
+  local len = 0
+  for _, v in ipairs(foldtext) do
+    len = len + #v[1]
   end
-
-  return result
+  return foldtext, len
 end
-
-function HighlightedFoldtext()
-  local result = parse_line(vim.v.foldstart)
-  if not result then
-    return vim.fn.foldtext()
-  end
-
-  local folded = {
-    -- { ' ', 'FoldedIcon' },
-    { ' ', 'FoldedIcon' },
-    { '+' .. vim.v.foldend - vim.v.foldstart .. ' lines', 'FoldedText' },
-    { ' ', 'FoldedIcon' },
-    -- { ' ', 'FoldedIcon' },
-  }
-
-  for _, item in ipairs(folded) do
-    table.insert(result, item)
-  end
-
-  local result2 = parse_line(vim.v.foldend)
-  if result2 then
-    local first = result2[1]
-    result2[1] = { vim.trim(first[1]), first[2] }
-    for _, item in ipairs(result2) do
-      table.insert(result, item)
-    end
-  end
-
-  return result
+function M.MyFoldText()
+  local bul = '•'
+  local ret = {}
+  local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+  local line = vim.fn.getline(vim.v.foldstart) --[[@as string]]
+  local ident = line:match('^[%s-]*')
+  local indent = #ident > 0 and bul:rep(#ident - 1) .. ' ' or ''
+  table.insert(ret, { indent })
+  local left, leftlen = M.GetTreesitterFoldText(#ident + 1, line)
+  vim.list_extend(ret, left)
+  local precent = (vim.v.foldend - vim.v.foldstart + 1) / vim.api.nvim_buf_line_count(0) * 100
+  local right = string.format(
+    ' %d lines:%3s%% %s',
+    vim.v.foldend - vim.v.foldstart + 1,
+    precent < 1 and tostring(precent):sub(2, 3) or math.floor(precent),
+    bul:rep(3)
+  )
+  local len = #vim.str_utf_pos(indent) + leftlen + wininfo.textoff + #vim.str_utf_pos(right)
+  local middle = bul:rep(wininfo.width - len)
+  table.insert(ret, { middle })
+  table.insert(ret, { right })
+  return ret
 end
-
-local function set_fold_hl()
-  local cl = vim.api.nvim_get_hl(0, { name = 'CursorLineNr' })
-  vim.api.nvim_set_hl(0, 'FoldedIcon', { fg = cl.bg })
-  vim.api.nvim_set_hl(0, 'FoldedText', { bg = cl.bg, fg = cl.fg, italic = true })
+function M.setup()
+  _G.MyFoldText = M.MyFoldText
+  vim.o['foldtext'] = 'v:lua.MyFoldText()'
 end
-
-set_fold_hl()
-
-vim.api.nvim_create_autocmd('ColorScheme', {
-  callback = set_fold_hl,
-})
-
-return 'luaeval("HighlightedFoldtext")()'
+return M
