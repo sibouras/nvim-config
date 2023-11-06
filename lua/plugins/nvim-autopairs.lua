a776e2657cfd4096bfad73f71640eeba4fd144b0
sibouras <sibouras@users.noreply.github.com>
2023-11-06 13:14:20 +0100

    feat(keymaps): toggle cmp and autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,8 +1,23 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
+  keys = '<M-a>',
   opts = {
     break_undo = false,
     fast_wrap = {},
   },
+  config = function(_, opts)
+    local autopairs = require('nvim-autopairs')
+    autopairs.setup(opts)
+
+    vim.keymap.set({ 'n', 'i' }, '<M-a>', function()
+      if autopairs.state.disabled == true then
+        autopairs.enable()
+        print('Enabled autopairs')
+      else
+        autopairs.disable()
+        print('Disabled autopairs')
+      end
+    end, { desc = 'Toggle auto pairs' })
+  end,
 }
