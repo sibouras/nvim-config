55186157431885cbf32625bcea7c09a281a49a95
ala <marzoukala2018@gmail.com>
2023-11-12 05:11:56 +0100

    feat(heirline): shortened relative filename instead of tail

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -160,16 +160,16 @@ return {
       provider = function(self)
         -- first, trim the pattern relative to the current directory. For other
         -- options, see :h filename-modifers
-        local filename = vim.fn.fnamemodify(self.filename, ':t')
+        local filename = vim.fn.fnamemodify(self.filename, ':.')
         if filename == '' then
           return '[No Name]'
         end
         -- now, if the filename would occupy more than 1/4th of the available
         -- space, we trim the file path to its initials
         -- See Flexible Components section below for dynamic truncation
-        if not conditions.width_percent_below(#filename, 0.25) then
-          filename = vim.fn.pathshorten(filename)
-        end
+        -- if not conditions.width_percent_below(#filename, 0.25) then
+        filename = vim.fn.pathshorten(filename)
+        -- end
         return filename
       end,
       hl = { fg = utils.get_highlight('Directory').fg },
