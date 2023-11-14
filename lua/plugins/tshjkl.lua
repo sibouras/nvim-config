57d16d4c8b8ccd84479dfa91d336efc14d65fbdc
sibouras <sibouras@users.noreply.github.com>
2023-11-14 06:13:09 +0100

    feat(plugins): tshjkl.nvim

---
+++ lua/plugins/tshjkl.lua
@@ -0,0 +1,24 @@
+return {
+  'gsuuon/tshjkl.nvim',
+  keys = '<M-v>',
+  opts = {
+    select_current_node = false, -- false to highlight only
+    marks = {
+      parent = {
+        hl_group = 'CursorLine',
+      },
+      child = {
+        hl_group = 'IlluminatedWord',
+      },
+      prev = {
+        hl_group = 'DiffDelete',
+      },
+      next = {
+        hl_group = 'DiffAdd',
+      },
+      current = {
+        hl_group = 'Visual',
+      },
+    },
+  },
+}
