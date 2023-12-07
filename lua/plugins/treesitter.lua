30456109181b7c9f49d1f0f2746eeec4a26e6cc4
ala <marzoukala2018@gmail.com>
2023-12-07 01:49:06 +0100

    fix: context_commentstring nvim-treesitter module is deprecated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -77,10 +77,6 @@ return {
         -- colors = {}, -- table of hex strings
         -- termcolors = {} -- table of colour name strings
       },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
       textobjects = {
         select = {
           enable = true,
