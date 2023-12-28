39a44634e4346c2caa01abba3f850f0fab7d44ed
sibouras <sibouras@users.noreply.github.com>
2023-12-28 14:56:51 +0100

    bufend: update to 2059e28 which includes a floating window and backup old version

--- lua/small/bufend.lua
+++ lua/small/bufend.lua
@@ -1,107 +1,161 @@
+-- from: https://github.com/altermo/small.nvim/blob/main/lua/small/bufend.lua
+-- last commit: 2059e28
 local M = {}
-M.locked_files = {}
-function M.get_list(key)
+function M.buf_get_file(buf)
+  local filepath = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ':p')
+  if vim.fn.filereadable(filepath) ~= 1 then
+    return
+  end
+  return filepath
+end
+function M.buf_get_lfile(buf)
+  return M.file_to_lfile(M.buf_get_file(buf))
+end
+function M.file_to_lfile(file)
+  -- return file:gsub(vim.fn.getcwd(), '.')
+  return vim.fn.fnamemodify(file, ':.') -- custom
+end
+M.marked_buf = {}
+function M.get_buf_list(key)
   return vim
     .iter(vim.api.nvim_list_bufs())
     :filter(vim.api.nvim_buf_is_loaded)
-    :map(function(v)
-      local filepath = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(v), ':p')
-      if vim.fn.filereadable(filepath) ~= 1 then
-        return
-      end
-      if key and key ~= vim.fn.fnamemodify(filepath, ':t'):sub(1, 1) then
-        return
-      end
-      return filepath
+    :filter(function(v)
+      local file = M.buf_get_file(v)
+      return file and ((not key) or key == vim.fn.fnamemodify(file, ':t'):sub(1, 1))
     end)
     :rev()
     :totable()
 end
-function M.unlock_file(key)
-  M.locked_files[key] = nil
+function M.mark_buf(key)
+  M.marked_buf[key] = vim.api.nvim_get_current_buf()
 end
-function M.lock_file(key)
-  require('small.lib.select')(M.get_list(), {
-    -- custom
-    format_item = function(file)
-      return vim.fn.fnamemodify(file, ':.')
-    end,
-  }, function(i)
-    M.locked_files[key] = i
-  end)
+function M.unmark_buf(key)
+  M.marked_buf[key] = nil
 end
-function M.goto_file(key)
-  local dict = M.get_list(key)
-  if M.locked_files[key] then
-    vim.cmd.edit(M.locked_files[key])
-  elseif #dict == 0 then
-    if not vim.regex('\\v[a-z.-_]'):match_str(key) then
-      return
-    end
-    local items = {}
-    for path, path_type in
-      vim.fs.dir(vim.uv.cwd(), {
-        skip = function(dir_name)
-          return dir_name ~= 'node_modules' and dir_name ~= '.git' and dir_name ~= '.docusaurus'
-        end,
-        depth = math.huge,
-      })
-    do
-      -- https://itecnote.com/tecnote/regex-how-to-write-this-regular-expression-in-lua/
-      -- the pattern %f[%w%-] matches all words which are preceded by something
-      -- that is neither a word nor a dash and [^/] matches any character that is not /
-      local case_insensite_key = '[' .. key .. string.upper(key) .. ']'
-      if path_type == 'file' and path:match('%f[%w%-]' .. case_insensite_key .. '[^/]+%.%w+$') then
-        table.insert(items, path)
-      end
-    end
-    require('small.lib.select')(
-      items,
-      -- or with fd(faster for large directory but starts slow)
-      -- vim.fn.glob(('`fd -t file -g %s*`'):format(key), true, true),
-      -- or with vim.fs.find(slow in large directory)
-      -- vim.fs.find(function(name, path)
-      --   return string.lower(name:sub(1, 1)) == key and not path:sub(#vim.fn.getcwd()):match('/%.')
-      -- end, { type = 'file', limit = math.huge }),
-      {
-        -- custom
-        -- format_item = function(file)
-        --   return vim.fn.fnamemodify(file, ':.')
-        -- end,
-      },
-      function(file)
-        -- custom
-        if file ~= nil then
-          vim.cmd.edit(vim.fn.fnamemodify(file, ':.'))
-        end
-      end
-    )
-  elseif #dict == 1 then
-    vim.cmd.edit(dict[1])
-  else
-    M.select(key)
-  end
-end
-function M.select(key)
-  require('small.lib.select')(M.get_list(key), {
-    format_item = function(file)
-      local relative_file = vim.fn.fnamemodify(file, ':.') -- custom
-      return vim.tbl_contains(M.locked_files, file) and '>> ' .. relative_file or relative_file
-    end,
-  }, function(file)
+function M.select(list)
+  require('small.lib.select')(vim.tbl_map(M.buf_get_lfile, list), {}, function(file)
     vim.cmd.edit(file)
   end)
 end
 function M.run()
-  local char = vim.fn.getcharstr()
-  if char == '\t' then
-    M.lock_file(vim.fn.getcharstr())
-  elseif char == '\x80kb' then
-    M.unlock_file(vim.fn.getcharstr())
-  elseif char == '\r' then
-    M.select()
-  elseif char ~= '' then
-    M.goto_file(char)
+  local keys = {}
+  for _, buf in pairs(M.get_buf_list()) do
+    local file = assert(M.buf_get_file(buf))
+    local key = vim.fn.fnamemodify(file, ':t'):sub(1, 1)
+    keys[key] = M.get_buf_list(key)
   end
+  for key, buf in pairs(M.marked_buf) do
+    keys[key] = { buf }
+  end
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.bo[buf].bufhidden = 'wipe'
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
+    '<space>: quick toggle buffer mark',
+    '<cr>   : search buffer files',
+    '<tab>  : toggle buffer mark (input)',
+    '',
+  })
+  for key, bufs in vim.spairs(keys) do
+    vim.api.nvim_buf_set_lines(
+      buf,
+      -1,
+      -1,
+      false,
+      { (M.marked_buf[key] and '#' or '') .. key .. ' : ' .. table.concat(vim.tbl_map(M.buf_get_lfile, bufs), ' ;; ') }
+    )
+  end
+
+  -- custom
+  local width = 70
+  local height = 15
+  local center_y = ((vim.opt.lines:get() - height) / 2) - vim.opt.cmdheight:get()
+  local center_x = (vim.opt.columns:get() - width) / 2
+  local win = vim.api.nvim_open_win(buf, false, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = center_x,
+    row = center_y,
+    focusable = false,
+    style = 'minimal',
+    border = 'rounded',
+    noautocmd = true,
+  })
+  vim.api.nvim_buf_set_lines(buf, -1, -1, false, { '* : find files starting with *' })
+  vim.cmd.redraw()
+  local key = vim.fn.getcharstr()
+  vim.api.nvim_win_close(win, true)
+  local curbuf = vim.api.nvim_get_current_buf()
+  if key == ' ' then
+    local file = M.buf_get_file(curbuf)
+    if not file then
+      return
+    end
+    key = vim.fn.fnamemodify(file, ':t'):sub(1, 1)
+    if M.marked_buf[key] == curbuf then
+      M.unmark_buf(key)
+    else
+      M.mark_buf(key)
+    end
+  elseif key == '\r' then
+    M.select(M.get_buf_list())
+  elseif key == '\t' then
+    key = vim.fn.getcharstr()
+    if not key:match('[%w_.-]') then
+      return
+    end
+    if M.marked_buf[key] == curbuf then
+      M.unmark_buf(key)
+    else
+      M.mark_buf(key)
+    end
+  elseif key ~= '\x1b' then
+    if keys[key] then
+      if curbuf == M.marked_buf[key] then
+        keys[key] = M.get_buf_list(key)
+      end
+      keys[key] = vim.tbl_filter(function(x)
+        return x ~= curbuf
+      end, keys[key])
+      if #keys[key] == 1 then
+        vim.cmd.buf(keys[key][1])
+      elseif #keys[key] == 0 then
+      else
+        M.select(keys[key])
+      end
+    elseif key:match('[%w_.-]') then
+      local cb = function(files)
+        if #files == 0 then
+          vim.notify('no files found starting with ' .. key, vim.log.levels.INFO)
+          return
+        end
+        if #files == 1 then
+          vim.cmd.edit(files[1])
+          return
+        end
+        require('small.lib.select')(vim.tbl_map(M.file_to_lfile, files), {}, function(file)
+          if not file then
+            return
+          end
+          vim.defer_fn(function()
+            vim.cmd.edit(file)
+          end, 50)
+        end)
+      end
+      if vim.fn.executable('fd') == 0 then
+        cb(vim.fs.find(function(name, path)
+          return name:sub(1, 1) == key and not path:sub(#vim.fn.getcwd()):match('/%.')
+        end, { type = 'file', limit = 1000 }))
+      else
+        vim.system({ 'fd', '-tfile', '-s', '-a', '^' .. key }, {}, function(ev)
+          vim.schedule_wrap(cb)(vim.split(ev.stdout, '\n', { trimempty = true }))
+        end)
+      end
+    end
+  end
+end
+if vim.dev then
+  M.run()
 end
 return M
