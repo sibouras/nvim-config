588e2fb29f2e73015537af52d4f03b7fd14dde3f
sibouras <sibouras@users.noreply.github.com>
2024-03-11 11:47:28 +0100

    plugins: add before.nvim

---
+++ lua/plugins/before.lua
@@ -0,0 +1,15 @@
+return {
+  'bloznelis/before.nvim',
+  enabled = true,
+  config = function()
+    local before = require('before')
+    before.setup()
+
+    -- Jump to previous entry in the edit history
+    vim.keymap.set('n', '<C-h>', before.jump_to_last_edit, {})
+    -- Jump to next entry in the edit history
+    vim.keymap.set('n', '<C-l>', before.jump_to_next_edit, {})
+    -- Move edit history to quickfix and open it (or telescope)
+    vim.keymap.set('n', 'mo', before.show_edits, {})
+  end,
+}
