0fcfe9b417ac1ee345d2cc3bcb8f93c95ae62689
sibouras <sibouras@users.noreply.github.com>
2024-05-06 05:33:47 +0100

    fix: use new `inlay_hint.enable()` signature

--- lua/utils/toggle.lua
+++ lua/utils/toggle.lua
@@ -58,7 +58,7 @@ function M.inlay_hints(buf, value)
     if value == nil then
       value = not ih.is_enabled(buf)
     end
-    ih.enable(buf, value)
+    ih.enable(value, { bufnr = buf })
   end
 end
 
