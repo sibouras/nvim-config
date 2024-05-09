48ed7df63dc6f1e1d9dcf45846ded62becae1221
sibouras <sibouras@users.noreply.github.com>
2024-05-09 16:57:23 +0100

    plugins: add profile.vim and YankAssassin.vim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,4 +8,6 @@ return {
   -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { 'airblade/vim-matchquote', event = 'VeryLazy' }, -- should be loaded after matchit plugin
   { 'utilyre/sentiment.nvim', event = 'VeryLazy', opts = {} },
+  { 'svban/YankAssassin.vim', event = 'VeryLazy' },
+  'stevearc/profile.nvim',
 }
