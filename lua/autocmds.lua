22d2adf7912113106153af9fe03d868ac2ad4d32
sibouras <sibouras@users.noreply.github.com>
2024-05-23 05:18:15 +0100

    autocmd to go to last loc when opening a buffer

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -25,6 +25,24 @@ augroup Mkdir
 augroup END
 ]])
 
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup('last_loc'),
+  callback = function(event)
+    local exclude = { 'gitcommit' }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
+      return
+    end
+    vim.b[buf].last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- keep window position when switching buffers
 -- https://stackoverflow.com/questions/4251533/vim-keep-window-position-when-switching-buffers
 -- https://vim.fandom.com/wiki/Avoid_scrolling_when_switch_buffers
