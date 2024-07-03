d072361facfe04ba270d89b856971ece67f308cb
sibouras <sibouras@users.noreply.github.com>
2024-07-03 12:04:33 +0100

    update lazy-lock and fix breaking changes

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,12 +1,14 @@
 return {
   'folke/trouble.nvim',
-  cmd = { 'TroubleToggle', 'Trouble' },
+  cmd = 'Trouble',
+  opts = {},
   keys = {
-    { '<leader>qq', '<cmd>TroubleToggle<cr>', desc = 'Toggle Trouble' },
-    { '<leader>qd', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
-    { '<leader>qw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
-    { '<leader>ql', '<cmd>TroubleToggle loclist<cr>', desc = 'Location List (Trouble)' },
-    { '<leader>qx', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix List (Trouble)' },
+    { '<leader>qq', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
+    { '<leader>qd', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
+    { '<leader>qs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
+    { '<leader>qr', '<cmd>Trouble lsp toggle focus=true<cr>', desc = 'Lsp References (Trouble)' },
+    { '<leader>ql', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+    { '<leader>qx', '<cmd>Trouble quickfix toggle<cr>', desc = 'Quickfix List (Trouble)' },
     {
       '<Down>',
       function()
@@ -22,7 +24,7 @@ return {
       '<Up>',
       function()
         if require('trouble').is_open() then
-          require('trouble').previous({ skip_groups = true, jump = false })
+          require('trouble').prev({ skip_groups = true, jump = false })
         else
           vim.cmd('-1')
         end
