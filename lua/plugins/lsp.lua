cfd9f7b26230e18cf18cdbbcbbacea0fb577f149
ala <marzoukala2018@gmail.com>
2024-09-09 05:25:44 +0100

    update lazy-lock and fix breaking changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -289,8 +289,8 @@ return {
         })
       end,
 
-      ['tsserver'] = function()
-        lspconfig.tsserver.setup({
+      ['ts_ls'] = function()
+        lspconfig.ts_ls.setup({
           -- autostart = false,
           on_attach = on_attach,
           capabilities = capabilities,
