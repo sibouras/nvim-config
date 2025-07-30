d1b7d2ffd0da733b557dc6c3b1363bb0fa895a68
sibouras <sibouras@users.noreply.github.com>
2025-07-30 20:48:59 +0100

    keymap to toggle markdown checkbox

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -11,3 +11,20 @@ map('n', '<S-Tab>', "<Cmd>call search('\\[[^]]*\\]([^)]\\+)', 'b')<CR>", opts)
 
 -- open url if markdown link is a url else `gf`
 map('n', '<CR>', ":lua require('essentials').go_to_url('start')<CR>", opts)
+
+-- from: https://github.com/antonk52/dot-files/blob/master/nvim/ftplugin/markdown.lua
+vim.keymap.set('n', '<leader>t', function()
+  -- save cursor position
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local content = vim.api.nvim_get_current_line()
+  local res = vim.fn.match(content, '\\[ \\]')
+  if res == -1 then
+    vim.fn.execute('.s/\\[[x~]\\]/[ ]')
+  else
+    vim.fn.execute('.s/\\[ \\]/[x]')
+  end
+  -- restore cursor position
+  vim.api.nvim_win_set_cursor(0, cursor)
+end, { buffer = 0, silent = true, desc = 'Toggle checkbox' })
+vim.keymap.set('n', 'j', 'gj', { buffer = 0 })
+vim.keymap.set('n', 'k', 'gk', { buffer = 0 })
