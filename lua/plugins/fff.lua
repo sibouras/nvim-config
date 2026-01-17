ecffd4bba09f6c180e7321dcc69b4e1dcdd605d4
sibouras <sibouras@users.noreply.github.com>
2026-01-17 12:00:36 +0100

    fff: add build function to download prebuilt binary

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,7 +1,14 @@
 return {
   'dmtrKovalenko/fff.nvim',
+  -- bad commit: https://github.com/dmtrKovalenko/fff.nvim/pull/175
+  commit = '3c76ba523f12356955f3207d61cdd7a7dee686a6',
   -- NOTE: build manually with `OPENSSL_NO_VENDOR=1 cargo build --release`
   -- build = 'cargo build --release',
+  build = function()
+    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+    require('fff.download').download_or_build_binary()
+  end,
   cmd = { 'FFFFind' },
   keys = {
     {
