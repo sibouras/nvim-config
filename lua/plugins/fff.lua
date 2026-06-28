79b990dcfe07385c531692545c211b0e7658a3ed
sibouras <sibouras@users.noreply.github.com>
2026-06-28 20:01:15 +0100

    fff: unpin commit, use new features

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -2,25 +2,30 @@ return {
   'dmtrKovalenko/fff.nvim',
   -- NOTE: build manually with `OPENSSL_NO_VENDOR=1 cargo build --release`
   -- build = 'cargo build --release',
-  -- https://github.com/dmtrKovalenko/fff.nvim/issues/228
-  commit = 'd7bc72786d4362ca70aa05d397f8d08bbaf39604',
   build = function()
     require('fff.download').download_binary()
   end,
   cmd = { 'FFFFind' },
   keys = {
+    -- stylua: ignore start
+    { 'ff', function() require('fff').find_files() end, desc = 'FFF: find files' },
+    { 'fg', function() require('fff').live_grep() end, desc = 'LiFFFe grep' },
+    -- stylua: ignore end
     {
-      'ff',
+      'fw',
       function()
-        require('fff').find_files()
+        require('fff').live_grep_under_cursor()
       end,
-      desc = 'FFF: find files',
+      mode = { 'n', 'x' },
+      desc = 'Search current word / selection',
     },
   },
   opts = {
     layout = {
       prompt_position = 'top', -- 'top' or 'bottom'
       preview_position = 'right', -- 'left', 'right', 'top', 'bottom'
+      flex = { size = 110, wrap = 'bottom' },
+      min_list_height = 6, --  do not display anything except the list below this threshold
     },
   },
 }
